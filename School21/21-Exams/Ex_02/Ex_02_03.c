7383847215be68c3d7edb5a5e1f8d21c46a6f54b
Gianname <fazalovrr@gmail.com>
2025-03-26 22:46:57 +0300

    c

--- School21/21-Exams/Ex_02/Ex_02_03.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main (void) {
-    printf("\"I'm Groot\" (c) Groot");  //  \"  - кавычка
-    return 0;
-}
--- School21/21-Exams/Ex_02/Ex_02_04.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main (void) {
-    
-    printf("C:\\Windows\\System32\\notepad.exe");
-    return 0;
-}
