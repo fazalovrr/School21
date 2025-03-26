8fb4112aeeaf1d06c9ffc01550cd23a76f9eafc3
Gianname <fazalovrr@gmail.com>
2025-03-26 22:43:43 +0300

    b

---
+++ School21/21-Exams/raund.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+    double test;
+    int res;
+    scanf("%lf", &test);
+    printf(".0lf - %.0lf\n", test);
+    printf("round - %.0lf\n", round(test));
+}
