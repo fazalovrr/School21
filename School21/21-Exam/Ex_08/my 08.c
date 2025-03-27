aa7b63d9967c6a80e00d2e872ed9f4c77d76706c
Gianname <fazalovrr@gmail.com>
2025-03-27 11:36:47 +0300

    new Ex

---
+++ School21/21-Exam/Ex_08/my 08.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+struct struc_students{
+    char name[100];
+    int rate;
+};
+ struct struc_students *student;
+
+    student=malloc(n*sizeof(struct struc_students));
+
+
+
+    return 0;
+}
+
+
